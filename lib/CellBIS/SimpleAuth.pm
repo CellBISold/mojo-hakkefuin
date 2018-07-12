4a6f87d814e2c4f1f6cb8e8d7081241052ef4b0b
Achmad Yusri Afandi <linuxer08@gmail.com>
2018-07-12 15:38:35 +0800

    Add file "lib/CellBIS/SimpleAuth.pm"

---
+++ lib/CellBIS/SimpleAuth.pm
@@ -0,0 +1,28 @@
+package CellBIS::SimpleAuth;
+use Mojo::Base -base;
+
+# ABSTRACT: Main Module Simple Authentication
+our $VERSION = '0.1';
+
+1;
+
+=encoding utf8
+
+=head1 NAME
+
+CellBIS::SimpleAuth - Mojolicious Simple Authentication
+
+=head1 DESCRIPTION
+
+The purpose of this module is to support SQL abstraction in L<Mojo::mysql>.
+This module inherits from L<Mojo::Base>
+
+=head1 AUTHOR
+
+Achmad Yusri Afandi, E<lt>yusrideb@cpan.orgE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2018 by Achmad Yusri Afandi
+
+=cut
