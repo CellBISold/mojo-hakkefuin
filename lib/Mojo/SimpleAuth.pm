f7fe45c46b3af115f1b1e5eddc073cbccd5e14cb
Achmad Yusri Afandi <linuxer08@gmail.com>
2018-07-24 06:11:02 +0800

    Move CellBIS/SimpleAuth.pm to "Mojo/SimpleAuth.pm"

--- lib/Mojo/SimpleAuth.pm
+++ lib/Mojo/SimpleAuth.pm
@@ -1,4 +1,4 @@
-package CellBIS::SimpleAuth;
+package Mojo::SimpleAuth;
 use Mojo::Base -base;
 
 # ABSTRACT: Main Module Simple Authentication
@@ -10,19 +10,22 @@ our $VERSION = '0.1';
 
 =head1 NAME
 
-CellBIS::SimpleAuth - Mojolicious Simple Authentication
+Mojo::SimpleAuth - Module for Mojolicious::Plugin::SimpleAuth
 
 =head1 DESCRIPTION
 
 The purpose of this module is to support SQL abstraction in L<Mojo::mysql>.
-This module inherits from L<Mojo::Base>
+This module inherits from L<Mojo::Base>.
 
 =head1 AUTHOR
 
-Achmad Yusri Afandi, E<lt>yusrideb@cpan.orgE<gt>
+Achmad Yusri Afandi, C<yusrideb@cpan.org>
 
 =head1 COPYRIGHT AND LICENSE
 
 Copyright (C) 2018 by Achmad Yusri Afandi
+
+This program is free software, you can redistribute it and/or modify it under
+the terms of the Artistic License version 2.0.
 
 =cut
