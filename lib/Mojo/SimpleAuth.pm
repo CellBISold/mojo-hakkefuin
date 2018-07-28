0e9d5473cb2a93f1e056b8296251c48ff0f421c8
Achmad Yusri Afandi <linuxer08@gmail.com>
2018-07-29 06:12:07 +0800

    Change attribute 'dbh' to 'sth' and updated POD

--- lib/Mojo/SimpleAuth.pm
+++ lib/Mojo/SimpleAuth.pm
@@ -13,7 +13,7 @@ use CellBIS::SQL::Abstract;
 
 # Attributes
 has random => sub { String::Random->new };
-has 'dbh';
+has 'sth';
 has 'via';
 has 'dir';
 has 'table_config';    # not yet implemented.
@@ -129,9 +129,74 @@ sub _via_db_pg {
 
 Mojo::SimpleAuth - Abstraction for L<Mojolicious::Plugin::SimpleAuth>
 
+=head1 SYNOPSIS
+
+  use Mojo::SimpleAuth;
+  
+  my $msa = Mojo::SimpleAuth->new({
+    sth => 'Mojo::SQLite',
+    via => 'db:sqlite',
+    dir => 'migrations'
+  });
+  $msa->prepare();
+
 =head1 DESCRIPTION
 
-General abstraction for L<Mojolicious::Plugin:::SimpleAuth>
+General abstraction for L<Mojolicious::Plugin:::SimpleAuth>.
+By defaults store handler using L<Mojo::SQLite>
+
+=head1 ATTRIBUTES
+
+L<Mojo::SimpleAuth> inherits all attributes from
+L<Mojo::Base> and implements the following new ones.
+
+=head2 sth
+
+  $msa->sth('Mojo::mysql');
+  $msa->sth('Mojo::SQLite');
+  $msa->sth('Mojo::Pg');
+  
+Specify of storage handler. Currently, available for L<Mojo::mysql>, L<Mojo::Pg>,
+and L<Mojo::SQLite>. By default using L<Mojo::SQLite>.
+
+=head2 via
+
+  $msa->via;
+  $msa->via('db:mysql');
+  $msa->via('db:sqlite');
+  $msa->via('db:pg');
+  
+Specify of handler via MariaDB/MySQL or SQLite or PostgreSQL.
+
+=head2 dir
+
+  $msa->dir;
+  $msa->dir('migration');
+  
+Specify the migration storage directory for L<Mojo::SimpleAuth> configuration file.
+
+=head1 METHODS
+
+L<Mojo::SimpleAuth> inherits all methods from
+L<Mojo::Base> and implements the following new ones.
+
+=head2 prepare()
+
+  $msa->prepare();
+  
+Setup storage handler.
+
+=head2 check_file_migration()
+
+  $msa->check_file_migration();
+  
+Checking file migration on your application directory.
+
+=head2 check_migration()
+
+  $msa->check_migration();
+  
+Checking migration database storage
 
 =head1 AUTHOR
 
