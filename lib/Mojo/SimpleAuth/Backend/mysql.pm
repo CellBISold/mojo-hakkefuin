4f6c68192567d30bdd1f78773a1a1370567fa9fc
Achmad Yusri Afandi <linuxer08@gmail.com>
2018-09-12 00:13:10 +0800

    Enable backend "mysql"

--- lib/Mojo/SimpleAuth/Backend/mysql.pm
+++ lib/Mojo/SimpleAuth/Backend/mysql.pm
@@ -1,73 +1,102 @@
 package Mojo::SimpleAuth::Backend::mysql;
-use Mojo::Base -base;
+use Mojo::Base 'Mojo::SimpleAuth::Backend';
 
-use Carp 'croak';
-use Scalar::Util 'blessed';
+use Mojo::mysql;
 use CellBIS::SQL::Abstract;
 
-has 'dbh' => sub { croak "Uninitialized Database Handler" unless shift->dbh };
-has 'dir';
-has 'app';
+has 'mysql';
+has 'file_migration';
 has abstract => sub { state $abstract = CellBIS::SQL::Abstract->new };
 
-has table_name  => 'mojo_simple_auth';
-has id          => 'id_auth';
-has identify    => 'identify';
-has cookie      => 'cookie';
-has create_date => 'create_date';
-has expire_date => 'expire_date';
-has status      => 'status';
+sub new {
+  my $self = shift->SUPER::new(@_);
 
-sub file_migration {
+  $self->file_migration($self->dir . '/msa_mysql.sql');
+  $self->mysql(Mojo::mysql->new($self->dsn()));
+
+  return $self;
+}
+
+sub check_table {
   my $self = shift;
-  return $self->dir . '/msa_mariadb.sql';
+
+  my $result = {result => 0, code => 400};
+  my $q = $self->abstract->select('information_schema.tables', ['table_name'],
+    {where => 'table_name=\'' . $self->table_name . '\''});
+
+  if (my $dbh = $self->mysql->db->query($q)) {
+    $result->{result} = $dbh->hash;
+    $result->{code}   = 200;
+  }
+  return $result;
 }
 
 sub create_table {
   my $self        = shift;
   my $table_query = $self->table_query;
 
-  my $result = $self->dbh->db->query($table_query);
-  return $self unless $result->rows;
+  my $result = {result => 0, code => 400};
 
-  return $result->rows;
+  if (my $dbh = $self->mysql->db->query($table_query)) {
+    $result->{result} = $dbh->rows;
+    $result->{code}   = 200;
+  }
+  return $result;
 }
 
 sub table_query {
   my $self = shift;
 
-  $self->abstract->new(db_type => 'sqlite')->create_table(
+  $self->abstract->new(db_type => 'mysql')->create_table(
     $self->table_name,
     [
       $self->id,          $self->identify,    $self->cookie,
-      $self->create_date, $self->expire_date, $self->status
+      $self->csrf,        $self->create_date, $self->expire_date,
+      $self->cookie_lock, $self->lock
     ],
     {
       $self->id =>
-        {type => {name => 'int'}, is_primarykey => 1, is_autoincre => 1},
+        {type => {name => 'integer'}, is_primarykey => 1, is_autoincre => 1},
       $self->identify    => {type => {name => 'text'}},
       $self->cookie      => {type => {name => 'text'}},
+      $self->csrf        => {type => {name => 'text'}},
       $self->create_date => {type => {name => 'datetime'}},
       $self->expire_date => {type => {name => 'datetime'}},
-      $self->status      => {type => {name => 'int'}},
+      $self->cookie_lock => {
+        type      => {name => 'varchar', size => 100},
+        'default' => '\'no_lock\'',
+        is_null   => 1
+      },
+      $self->lock => {type => {name => 'int'}}
     }
   );
 }
 
 sub create {
-  my ($self, $identify, $cookie) = @_;
+  my ($self, $identify, $cookie, $csrf, $expires) = @_;
 
-  my $result = {result => 0, data => $cookie};
-  my $q = $self->abstract->insert(
+  return {result => 0, code => 500, data => $cookie}
+    unless $self->check_table->{result};
+
+  my $result = {result => 0, code => 400, data => $cookie};
+
+  my $msa_utils   = $self->msa_util->new;
+  my $now_time    = $msa_utils->sql_datetime(0);
+  my $expire_time = $msa_utils->sql_datetime($expires);
+  my $q           = $self->abstract->insert(
     $self->table_name,
     [
-      $self->identify,    $self->cookie, $self->create_date,
-      $self->expire_date, $self->status
+      $self->identify,    $self->cookie,      $self->csrf,
+      $self->create_date, $self->expire_date, $self->cookie_lock,
+      $self->lock
     ],
-    [$identify, $cookie, 'NOW()', 'NOW()', 0]
+    [$identify, $cookie, $csrf, $now_time, $expire_time, $now_time, 0]
   );
-  if (my $dbh = $self->dbh->db->query($q)) {
+
+  # warn $q;
+  if (my $dbh = $self->mysql->db->query($q)) {
     $result->{result} = $dbh->rows;
+    $result->{code}   = 200;
   }
   return $result;
 }
@@ -75,45 +104,200 @@ sub create {
 sub read {
   my ($self, $identify, $cookie) = @_;
 
-  $identify //= '';
-  $cookie   //= '';
+  $identify //= 'null';
+  $cookie   //= 'null';
 
-  my $result = {result => 0, data => $cookie};
-  my $q = $self->abstract->select($self->table_name, [],
-    {where => "$self->identify = '$identify' OR $self->cookie = '$cookie'"});
-  if (my $dbh = $self->dbh->db->query($q)) {
-    $result->{result} = $dbh->rows;
+  return {result => 0, code => 500, data => $cookie}
+    unless $self->check_table->{result};
+
+  my $result = {result => 0, code => 400, data => $cookie};
+  my $q = $self->abstract->select(
+    $self->table_name,
+    [],
+    {
+          where => $self->identify
+        . " = '$identify' AND "
+        . $self->cookie
+        . " = '$cookie'"
+    }
+  );
+  if (my $dbh = $self->mysql->db->query($q)) {
+    $result->{result} = 1;
+    $result->{code}   = 200;
+    $result->{data}   = $dbh->hash;
   }
   return $result;
 }
 
 sub update {
+  my ($self, $id, $cookie, $csrf) = @_;
+
+  my $msa_utils = $self->msa_util->new;
+  my $now_time  = $msa_utils->sql_datetime(0);
+
+  return {result => 0, code => 500, csrf => $csrf, cookie => $cookie}
+    unless $self->check_table->{result};
+
+  my $result = {result => 0, code => 400, csrf => $csrf, cookie => $cookie};
+  return $result unless $id && $csrf;
+
+  my $q = $self->abstract->update(
+    $self->table_name,
+    {$self->cookie, => $cookie, $self->csrf => $csrf},
+    {
+          where => '('
+        . $self->id
+        . " = '$id' OR "
+        . $self->identify
+        . " = '$id') AND "
+        . $self->expire_date
+        . " > '$now_time'"
+    }
+  );
+  if (my $dbh = $self->mysql->db->query($q)) {
+    $result->{result} = $dbh->rows;
+    $result->{code}   = 200;
+  }
+  return $result;
+}
+
+sub update_csrf {
+  my ($self, $id, $csrf) = @_;
+
+  my $msa_utils = $self->msa_util->new;
+  my $now_time  = $msa_utils->sql_datetime(0);
+
+  return {result => 0, code => 500, data => $csrf}
+    unless $self->check_table->{result};
+
+  my $result = {result => 0, code => 400, data => $csrf};
+  return $result unless $id && $csrf;
+
+  my $q = $self->abstract->update(
+    $self->table_name,
+    {$self->csrf => $csrf},
+    {
+          where => $self->id
+        . " = '$id' AND "
+        . $self->expire_date
+        . " > '$now_time'"
+    }
+  );
+  if (my $dbh = $self->mysql->db->query($q)) {
+    $result->{result} = $dbh->rows;
+    $result->{code}   = 200;
+  }
+  return $result;
+}
+
+sub update_cookie {
   my ($self, $id, $cookie) = @_;
 
-  $id     //= 'null';
-  $cookie //= 'null';
+  my $msa_utils = $self->msa_util->new;
+  my $now_time  = $msa_utils->sql_datetime(0);
 
-  my $result = {result => 0, data => $cookie};
-  my $q = $self->abstract->update($self->table_name, [$self->cookie], [$cookie],
-    where => "$self->id = '$id' OR $self->cookie = '$cookie'");
-  if (my $dbh = $self->dbh->db->query($q)) {
+  return {result => 0, code => 500, data => $cookie}
+    unless $self->check_table->{result};
+
+  my $result = {result => 0, code => 400, data => $cookie};
+  return $result unless $id && $cookie;
+
+  my $q = $self->abstract->update(
+    $self->table_name,
+    {$self->cookie => $cookie},
+    {
+          where => $self->id
+        . " = '$id' AND "
+        . $self->expire_date
+        . " > '$now_time'"
+    }
+  );
+  if (my $dbh = $self->mysql->db->query($q)) {
     $result->{result} = $dbh->rows;
+    $result->{code}   = 200;
   }
   return $result;
 }
 
 sub delete {
-  my ($self, $identify, $cookie) = @_;
+  my ($self, $id, $cookie) = @_;
 
-  $identify //= '';
-  $cookie   //= '';
+  return {result => 0, code => 500, data => $cookie}
+    unless $self->check_table->{result};
 
-  my $result = {result => 0, data => $cookie};
-  my $q = $self->abstract->delete($self->table_name, [],
-    {where => $self->identify . " = '$identify' AND $self->cookie = '$cookie'"}
-  );
-  if (my $dbh = $self->dbh->db->query($q)) {
+  my $result = {result => 0, code => 400, data => $cookie};
+  return $result unless $id && $cookie;
+
+  my $q = $self->abstract->delete($self->table_name,
+    {where => $self->identify . " = ? AND " . $self->cookie . " = ?"});
+  if (my $dbh = $self->mysql->db->query($q, $id, $cookie)) {
     $result->{result} = $dbh->rows;
+    $result->{code}   = 200;
+  }
+  return $result;
+}
+
+sub check {
+  my ($self, $id, $cookie) = @_;
+
+  return {result => 0, code => 500, data => $cookie}
+    unless $self->check_table->{result};
+
+  my $result = {result => 0, code => 400, data => $cookie};
+  return $result unless $id && $cookie;
+
+  my $q = $self->abstract->select(
+    $self->table_name,
+    [],
+    {
+      where => '('
+        . $self->identify
+        . " = '$id' OR "
+        . $self->cookie
+        . " = '$cookie') AND "
+        . $self->expire_date
+        . " > NOW()",
+      limit => 1
+    }
+  );
+  if (my $rv = $self->mysql->db->query($q)) {
+    my $r_data = $rv->hash;
+    $result = {
+      result => 1,
+      code   => 200,
+      data   => {
+        cookie   => $cookie,
+        id       => $r_data->{$self->id},
+        csrf     => $r_data->{$self->csrf},
+        identify => $r_data->{$self->identify}
+      }
+    };
+  }
+  return $result;
+}
+
+sub empty_table {
+  my $self = shift;
+  my $result = {result => 0, code => 500, data => 'can\'t delete table'};
+
+  if (my $dbh = $self->mysql->db->query('DELETE FROM ' . $self->table_name)) {
+    $result->{result} = $dbh->rows;
+    $result->{code}   = 200;
+    $result->{data}   = '';
+  }
+  return $result;
+}
+
+sub drop_table {
+  my $self = shift;
+  my $result = {result => 0, code => 500, data => 'can\'t drop table'};
+
+  if (my $dbh
+    = $self->mysql->db->query('DROP TABLE IF EXISTS ' . $self->table_name))
+  {
+    $result->{result} = $dbh->rows;
+    $result->{code}   = 200;
+    $result->{data}   = '';
   }
   return $result;
 }
